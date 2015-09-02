bdd079fa2e9327502cebf2f9c9f1579804b72641
d-smith <doug.smith.mail@gmail.com>
2015-09-02 12:03:43 -0600

    Added auth param validation

--- http/authz.go
+++ http/authz.go
@@ -27,28 +27,58 @@ func handleAuthorize(core *roll.Core) http.Handler {
 
 func requiredQueryParamsPresent(r *http.Request) bool {
 	params := r.URL.Query()
-	if _, ok := params["client_id"]; !ok {
-		return false
-	}
+	required := []string{"client_id","redirect_uri","response_type"}
+	for _,v := range required {
+		if vals, ok := params[v]; !ok || len(vals) > 1 {
+			return false
+		}
 
-	if _, ok := params["redirect_uri"]; !ok {
-		return false
-	}
-
-	if _, ok := params["response_type"]; !ok {
-		return false
 	}
 
 	return true
+}
+
+func validateInputParams(core *roll.Core, r *http.Request) error {
+	params := r.URL.Query()
+
+	if params["response_type"][0] != "token" {
+		return errors.New("Only token is support for response_type")
+	}
+
+
+	//Client id is application key
+	app, err := core.RetrieveApplication(params["client_id"][0])
+	if err != nil {
+		return err
+	}
+
+	if app == nil {
+		return errors.New("Invalid client id")
+	}
+
+
+	if app.RedirectUri != params["redirect_uri"][0] {
+		return errors.New("redirect_uri does not match registered redirect URIs")
+	}
+
+	return nil
 }
 
 func handleAuthZGet(core *roll.Core, w http.ResponseWriter, r *http.Request) {
 
 	//Check the query params
 	if !requiredQueryParamsPresent(r) {
-		respondError(w, http.StatusMethodNotAllowed, errors.New("Missing required query params"))
+		respondError(w, http.StatusBadRequest, errors.New("Missing required query params or multiple values for single param"))
 		return
 	}
+
+	//Validate client_id and redirect_uri
+	err := validateInputParams(core, r)
+	if err != nil {
+		respondError(w, http.StatusBadRequest,err)
+		return
+	}
+
 
 	respondOk(w, nil)
 }
