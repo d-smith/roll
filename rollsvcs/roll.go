edd61b12c7534c8100192a02e4e3fba7d4beca8a
d-smith <doug.smith.mail@gmail.com>
2015-09-02 09:11:49 -0600

    Added port flag to rollsvcs

--- rollsvcs/roll.go
+++ rollsvcs/roll.go
@@ -6,9 +6,19 @@ import (
 	"log"
 	"net/http"
 	"github.com/xtraclabs/roll/repos"
+	"flag"
+	"fmt"
 )
 
 func main() {
+
+	var port = flag.Int("port", -1, "Port to listen on")
+	flag.Parse()
+	if *port == -1 {
+		fmt.Println("Must specify a -port argument")
+		return
+	}
+
 	var coreConfig = roll.CoreConfig{
 		DeveloperRepo: repos.NewDynamoDevRepo(),
 		ApplicationRepo: repos.NewDynamoAppRepo(),
@@ -17,6 +27,6 @@ func main() {
 
 	core := roll.NewCore(&coreConfig)
 
-	log.Println("Listening on port 12345")
-	http.ListenAndServe(":12345", rollhttp.Handler(core))
+	log.Println("Listening on port ",*port)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), rollhttp.Handler(core))
 }
