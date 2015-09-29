13da6a0ea5099c83ced2181dbe315f63da64c58d
d-smith <doug.smith.mail@gmail.com>
2015-09-29 12:38:34 -0600

    Added developer service test coverage

--- http/developers_test.go
+++ http/developers_test.go
@@ -28,6 +28,34 @@ func TestStoreDeveloper(t *testing.T) {
 	checkResponseStatus(t, resp, http.StatusNoContent)
 }
 
+func TestStoreDeveloperInvalidEmailResource(t *testing.T) {
+	core, _ := NewTestCore()
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	resp := testHTTPPut(t, addr+"/v1/developers/<script/>", nil)
+	checkResponseStatus(t, resp, http.StatusBadRequest)
+}
+
+func TestGetDeveloperInvalidEmailResource(t *testing.T) {
+	core, _ := NewTestCore()
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	resp := testHTTPGet(t, addr+"/v1/developers/<script/>", nil)
+	checkResponseStatus(t, resp, http.StatusBadRequest)
+}
+
+func TestDeveloperUnsupportedMethod(t *testing.T) {
+	core, _ := NewTestCore()
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	resp, err := http.Post(addr+"/v1/developers/1111-2222-3333333-4444444", "application/json", nil)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusMethodNotAllowed, resp.StatusCode)
+}
+
 func TestGetDeveloper(t *testing.T) {
 	core, coreConfig := NewTestCore()
 	ln, addr := TestServer(t, core)
@@ -45,5 +73,20 @@ func TestGetDeveloper(t *testing.T) {
 	assert.Equal(t, "Joe", actual.FirstName)
 	assert.Equal(t, "Dev", actual.LastName)
 	assert.Equal(t, "joe@dev.com", actual.Email)
+
+}
+
+func TestGetNonExistentDeveloper(t *testing.T) {
+	core, coreConfig := NewTestCore()
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	devRepoMock := coreConfig.DeveloperRepo.(*mocks.DeveloperRepo)
+	devRepoMock.On("RetrieveDeveloper", "joe@dev.com").Return(nil, nil)
+
+	resp := testHTTPGet(t, addr+"/v1/developers/joe@dev.com", nil)
+	devRepoMock.AssertCalled(t, "RetrieveDeveloper", "joe@dev.com")
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 
 }
